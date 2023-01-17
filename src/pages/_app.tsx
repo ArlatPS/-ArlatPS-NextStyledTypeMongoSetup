493ab14c5480d7df8dc7c86791208c04dd74e0ef
ArlatPS <przemekx10@gmail.com>
2023-01-17 14:14:03 +0100

    file-download

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import GlobalStyle from "../../GlobalStyle.js";
+import GlobalStyle from "../../GlobalStyle";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
